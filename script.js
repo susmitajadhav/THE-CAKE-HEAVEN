f848dd73f99410be98c5b7f59b16c56b805ca583
Yash Hulle <150221594+INFINITYASH3699@users.noreply.github.com>
2024-06-27 00:54:26 +0530

    Added Dark mode in home page for the trial

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleSwitch = document.getElementById('darkModeSwitch');
+
+  function applyDarkModePreference() {
+      if (localStorage.getItem('darkMode') === 'enabled') {
+          document.body.classList.add('dark-mode');
+          toggleSwitch.checked = true;
+      } else {
+          document.body.classList.remove('dark-mode');
+          toggleSwitch.checked = false;
+      }
+  }
+
+  function toggleDarkMode() {
+      if (toggleSwitch.checked) {
+          document.body.classList.add('dark-mode');
+          localStorage.setItem('darkMode', 'enabled');
+      } else {
+          document.body.classList.remove('dark-mode');
+          localStorage.setItem('darkMode', 'disabled');
+      }
+  }
+
+  toggleSwitch.addEventListener('change', toggleDarkMode);
+  applyDarkModePreference();
+});
+
+
 $(document).ready(function () {
   $("#showLoginForm").on("click", function () {
     $("#signupForm").hide();
