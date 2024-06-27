82dd8557bd5d501c8ceeea7d309a142e1b9c9240
Yash Hulle <150221594+INFINITYASH3699@users.noreply.github.com>
2024-06-28 00:08:33 +0530

    New review component added and dark mode optimized and applied to all pages

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleSwitch = document.getElementById('darkModeSwitch');
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleSwitch = document.getElementById("darkModeSwitch");
 
   function applyDarkModePreference() {
-      if (localStorage.getItem('darkMode') === 'enabled') {
-          document.body.classList.add('dark-mode');
-          toggleSwitch.checked = true;
-      } else {
-          document.body.classList.remove('dark-mode');
-          toggleSwitch.checked = false;
-      }
+    if (localStorage.getItem("darkMode") === "enabled") {
+      document.body.classList.add("dark-mode");
+      toggleSwitch.checked = true;
+    } else {
+      document.body.classList.remove("dark-mode");
+      toggleSwitch.checked = false;
+    }
   }
 
   function toggleDarkMode() {
-      if (toggleSwitch.checked) {
-          document.body.classList.add('dark-mode');
-          localStorage.setItem('darkMode', 'enabled');
-      } else {
-          document.body.classList.remove('dark-mode');
-          localStorage.setItem('darkMode', 'disabled');
-      }
+    if (toggleSwitch.checked) {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("darkMode", "disabled");
+    }
   }
 
-  toggleSwitch.addEventListener('change', toggleDarkMode);
+  toggleSwitch.addEventListener("change", toggleDarkMode);
   applyDarkModePreference();
 });
-
 
 $(document).ready(function () {
   $("#showLoginForm").on("click", function () {
